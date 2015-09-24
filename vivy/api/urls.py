91a2aec09bb506859e89bba9fedebc6daf89f4f1
campbea <campbellandrewaof@yahoo.com>
2015-09-24 14:23:18 -0500

    add employee tests

--- vivy/api/urls.py
+++ vivy/api/urls.py
@@ -5,9 +5,9 @@ from .projects_api import ListCreateProject, RetrieveUpdateDestroyProject
 
 urlpatterns = patterns('',
     # Project Routes
-    url(r'^v1/project/$', ListCreateProject.as_view(), name='project_list_create'),
+    url(r'^v1/project/$', ListCreateProject.as_view(), name='list_create_project'),
     url(r'^v1/project/(?P<pk>\d+)/$', RetrieveUpdateDestroyProject.as_view(), name='retrieve_update_destroy_project'),
     # Employee routes
-    url(r'^v1/employee/$', ListCreateEmployee.as_view(), name='employee_list_create'),
+    url(r'^v1/employee/$', ListCreateEmployee.as_view(), name='list_create_employee'),
     url(r'^v1/employee/(?P<pk>\d+)/$', RetrieveUpdateDestroyEmployee.as_view(), name='retrieve_update_destroy_employee'),
 )
